f8fd2a9912c90e918410e590b9f06e1e48e63fb7
Aditya Sahu <adityaonx@gmail.com>
2023-10-20 02:06:31 +0530

    1st Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="divbody">
+      <img src="./src/assets/profilepic.jpg" alt="Aditya" />
+      <p className="name">Aditya Sahu</p>
+      <p className="bodycontent">
+        Front-End Developer Enthusiast who is passionate about building
+        intuitive, user-friendly experiences on the web. I love exploring the
+        latest technologies and leveraging them to solve real-world problems.
+      </p>
+      <ul className="hlist">
+        <li>HTML+CSS💪</li>
+        <li>Javascript💪</li>
+        <li>Web Design💪</li>
+        <li>Git and Github👍</li>
+        <li />
+        <li>React💪</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
